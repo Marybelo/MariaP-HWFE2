70b061e4d0d17eda78d69367e6f6a250d1f0e339
Marybelo <marybelo.web@gmail.com>
2020-06-08 14:40:43 +0300

    add hw

--- class17_js/main.js
+++ class17_js/main.js
@@ -1,14 +1,25 @@
 // 1. Создайте функцию, которая получает в качестве аргументов три числа и возвращает наименьшее из них.
 
+// function minNum(a, b, c) {
+//     let result = c;
+//     if (a < b && a < c) {
+//         result = a;
+//     }
+//     if (b < a && b < c) {
+//         result = b;
+//     }
+//     return result
+// }
+
 function minNum(a, b, c) {
-    let result = c
+
     if (a < b && a < c) {
-        result = a;
+        return a;
     }
     if (b < a && b < c) {
-        result = b;
+        return b;
     }
-    return result
+    return c
 }
 console.log("Первое мин", minNum(1, 2, 3));
 console.log("Второе мин", minNum(2, 1, 3));
@@ -20,24 +31,43 @@ console.log("Третье мин", minNum(3, 2, 1));
 
 
 function time(days, hours, minutes) {
-    let res = days * 86400 + hours * 3600 + minutes * 60;
-    return res
+    return days * 60 * 60 * 24 + hours * 60 * 60 + minutes * 60;
 }
 
 console.log(time(1, 0, 0));
 
 
+let time = function (days, hours, minutes) {
+    return days * 60 * 60 * 24 + hours * 60 * 60 + minutes * 60;
+}
+
+console.log(time(1, 0, 0));
+
+
+let time = (days, hours, minutes) =>
+    days * 60 * 60 * 24 + hours * 60 * 60 + minutes * 60;
+
+console.log(time(1, 0, 0));
+
+
+let squareNum = a => a * a;
+
+function squareNum(a) {
+    return a * a
+}
+
+
 // 3. Напишите рекурсивную функцию для подсчёта факториала числа.
 
 
-function fac (n) {
-let res = 1;
-for (let i = 2; i <= n; i ++) {
-    res = res * i;
+function fac(n) {
+    let res = 1;
+    for (let i = 2; i <= n; i++) {
+        res = res * i;
+    }
+    return res;
 }
-return res;
-}
-console.log (fac(4));
+console.log(fac(4));
 
 
 // 4. Напишите рекурсивную функцию, которая разворачивает введённое число задом наперёд. 
