a0ee00ed906f80a94f506eb96c7edf9ea89d4068
Marybelo <marybelo.web@gmail.com>
2020-07-03 16:25:54 +0300

    add hw

---
+++ class18_js/callprint.js
@@ -0,0 +1,12 @@
+let animals =[
+    {species: 'lion', name: 'king'},
+    {species: 'whale', name: 'moby dick'},
+    {species: 'cat', name: 'cheshire  cat'}
+];
+
+animals.forEach(function(elem,index) {
+    
+}
+
+
+)
